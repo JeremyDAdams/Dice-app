b7b8c2a0d114fe9807682b594c180a73aa95f84b
Jeremy Adams <Beoron@gmail.com>
2019-07-05 22:39:09 -0500

    Add GetDice component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import Die from './Die';
+import RollDice from './RollDice';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Die face="five" />
-      <Die face="four" />
-      <Die face="six" />
-      <Die face="five" />
+      <RollDice />
     </div>
   );
 }
